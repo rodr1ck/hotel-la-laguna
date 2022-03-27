351d290fde525f6e2180f4e09db514e5c482f148
rodrigodor <rodrigo.orelana@gmail.com>
2022-03-27 10:19:44 -0300

    agrega uso de pipe con fecha y en español Chile

--- client/src/app/pasajeros/pasajeros.module.ts
+++ client/src/app/pasajeros/pasajeros.module.ts
@@ -1,11 +1,13 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { LOCALE_ID, NgModule } from '@angular/core';
+import { CommonModule, registerLocaleData } from '@angular/common';
 
 import { PasajerosRoutingModule } from './pasajeros-routing.module';
 import { NuevoComponent } from './pages/nuevo/nuevo.component';
 import { EditarComponent } from './pages/editar/editar.component';
 import { ListadoComponent } from './pages/listado/listado.component';
+import localeEsCL from '@angular/common/locales/es-CL';
 
+registerLocaleData(localeEsCL);
 
 @NgModule({
   declarations: [
@@ -16,6 +18,7 @@ import { ListadoComponent } from './pages/listado/listado.component';
   imports: [
     CommonModule,
     PasajerosRoutingModule
-  ]
+  ],
+	providers: [{ provide: LOCALE_ID, useValue: 'es-CL' }],
 })
 export class PasajerosModule { }
